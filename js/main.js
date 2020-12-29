116482907eb1544ddf352c0a2c4d7df864037d69
fajar27 <muh.fajar.prawira@student.untan.ac.id>
2020-12-29 10:51:48 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// Sidenav
+const sidenav = document.querySelectorAll('.sidenav');
+M.Sidenav.init(sidenav);
+
+const slider = document.querySelectorAll('.slider');
+M.Slider.init(slider, {
+	indicators: false,
+	height: 500,
+	transition: 600,
+	interval: 3000
+});
+
+// Parralax
+const parallax = document.querySelectorAll('.parallax');
+M.Parallax.init(parallax);
+
+// Material Box
+const materialbox = document.querySelectorAll('.materialboxed');
+M.Materialbox.init(materialbox);
+
+// scrollspy
+const scroll = document.querySelectorAll('.scrollspy');
+M.ScrollSpy.init(scroll, {
+	scrollOffset: 50
+});
