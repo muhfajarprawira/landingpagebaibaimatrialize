19bab74ce6a05a32b0a774b0939f6c26220885a9
fajar27 <muh.fajar.prawira@student.untan.ac.id>
2020-12-31 08:54:33 +0700

    add fitur

--- js/main.js
+++ js/main.js
@@ -11,6 +11,51 @@ M.Slider.init(slider, {
 	interval: 3000
 });
 
+// Class Active
+// const currentLocation = location.href;
+// const menuItem = document.querySelectorAll('a');
+// const menuLength = menuItem.length
+// for (let i = 0; i < menuLength; i++) {
+// 	if (menuItem[i].href === currentLocation) {
+// 		menuItem[i].className = "active"
+// 	}
+// }
+
+
+// SCROLL SECTIONS ACTIVE LINK
+// const sections = document.querySelectorAll('section[id]')
+
+// window.addEventListener('scroll', scrollActive)
+
+// function scrollActive() {
+// 	const scrollY = window.pageYOffset
+
+// 	sections.forEach(current => {
+// 		const sectionHeight = current.offsetHeight
+// 		const sectionTop = current.offsetTop - 50
+// 		sectionId = current.getAttribute('id')
+
+// 		if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+// 			document.querySelector('a[href*=' + sectionId + ']').classList.add('active')
+// 		} else {
+// 			document.querySelector('a[href*=' + sectionId + ']').classList.remove('active')
+// 		}
+// 	})
+// }
+
+// Active On Click
+let buttons = document.querySelectorAll('ul li .a')
+
+buttons.forEach(button => {
+	buttons.addEventListener('click', function () {
+		buttons.forEach(btn => btn.classList.remove('active'));
+		this.classList.add('active');
+
+
+	})
+})
+
+
 // Parralax
 const parallax = document.querySelectorAll('.parallax');
 M.Parallax.init(parallax);
